ce98649550a9da5866895c9730b61bb880ae4633
Moses <asugam9@gmail.com>
2024-12-04 22:39:09 +0300

    cOLOR

---
+++ Dom Project 2/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const colorBox = document.getElementById('color-box');
+    const changeColor = document.getElementById('change-color-btn');
+
+    function getRandomColor(){
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for(let i = 0; i < 6; i++){
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color
+    }
+    changeColor.addEventListener('click', function(){
+        colorBox.style.backgroundColor = getRandomColor();
+    });
+});
